67eabb25388a1b6fc9f93cfe87dc8ac1786622dd
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2024-02-26 20:34:55 +0000

    Add styling and content for features container

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import './features.css';
+import { Feature } from '../../components';
+
+const featuresData = [
+  {
+    title: 'Improving end distrusts instantly',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.'
+  },
+  {
+    title: 'Become the tended active',
+    text: 'Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.'
+  },
+  {
+    title: 'Message or am nothing',
+    text: 'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.'
+  },
+  {
+    title: 'Really boy law county',
+    text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.'
+  }
+]
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <div id="features" className='gpt3__features section__padding'>
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'>
+          The future is now and you just need to realise it.
+          Step into the future today & make it happen.
+        </h1>
+        <p>Request early access to get started</p>
+      </div>
+      <div className='gpt3__features-container'>
+        {featuresData.map((feature, index) => (
+          <Feature title={feature.title} text={feature.text} key={feature.index} />
+        ))}
+      </div>
+    </div>
   )
 }
 
